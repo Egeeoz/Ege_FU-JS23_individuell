c80da73d29eba66d43f614ccc02baf361260ad8a
Egeeoz <ozakdemirege@hotmail.com>
2024-03-10 18:41:06 +0100

    Added content to status page and styled it

--- src/pages/StatusPage.jsx
+++ src/pages/StatusPage.jsx
@@ -1,9 +1,27 @@
 import Layout from "../Layout";
+import "../styles/statusPage.css";
+import { Link } from "react-router-dom";
 
 const StatusPage = () => {
   return (
     <Layout>
-      <div>StatusPage</div>
+      <main className="status-page-container">
+        <section className="top-part">
+          <p className="order-number">Ordernummer #12DV23F</p>
+          <div className="drone-img"></div>
+        </section>
+
+        <section className="bottom-part">
+          <h1 className="delivery-title">Din beställning är på väg!</h1>
+          <p className="delivery-time">
+            <span className="delivery-minutes">13</span> minuter
+          </p>
+        </section>
+
+        <Link to="/" className="ok-button">
+          Ok, cool!
+        </Link>
+      </main>
     </Layout>
   );
 };
